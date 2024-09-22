5bd4784d27cb39c49ac6a9772df5f70c25db9787
LuisRoj <luisrojas150520@gmail.com>
2024-09-22 10:44:13 -0500

    ejercicio x puntos

---
+++ Catalogo.java
@@ -0,0 +1,18 @@
+package com.example.ejerciciopts;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Catalogo {
+
+    private int idCatalogo;
+    private String descripcion;
+}
